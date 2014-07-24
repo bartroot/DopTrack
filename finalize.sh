dc39e53cee63ec67db8e7cac3bcc1d91234f66a2
Joao Encarnacao <j.g.deteixeiradaencarnacao@tudelft.nl>
2014-07-25 00:01:34 +0100

    updated budget, added schedule, added one more student report

--- finalize.sh
+++ finalize.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -u
 
 PROJECT="`cat .projectname`.pdf"
-APPENDIX="D14_ScientificArticleFinal.pdf"
+APPENDIX="D14_ScientificArticleFinal.pdf A6_final.pdf"
 FINAL="`cat .projectname`.final.pdf"
 
 pdftk $PROJECT $APPENDIX output $FINAL
