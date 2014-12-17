e1adcac3b4c639113718942a41b86d2ada65a51e
jgte <j.g.deteixeiradaencarnacao@tudelft.nl>
2014-12-17 18:08:25 +0100

    continued developing NPS-230.power_switch/libpowerswitch.rb

--- dev/NPS-230.power_switch/libpowerswitch.rb
+++ dev/NPS-230.power_switch/libpowerswitch.rb
@@ -73,5 +73,5 @@ end
 
 #run status if this file is called explicitly
 if __FILE__==$0
-  puts PowerSwitch::Control.new.cmd(:status).power("Off",2).cmd(:status).wait(5).cmd(:status).power("On",2).cmd(:status).wait(1).cmd(:status).close
+  puts PowerSwitch::Control.new.cmd(:status).power("Off",2).cmd(:status).power("On",2).cmd(:status).close
 end
