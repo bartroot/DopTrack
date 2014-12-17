29c24d50f2b87ec6339bc7c3ce970b296695a3ba
jgte <j.g.deteixeiradaencarnacao@tudelft.nl>
2014-12-17 16:40:54 +0100

    continued developing NPS-230.power_switch/libpowerswitch.rb

--- dev/NPS-230.power_switch/libpowerswitch.rb
+++ dev/NPS-230.power_switch/libpowerswitch.rb
@@ -4,16 +4,51 @@ require 'net/telnet'
 require 'rubygems'
 require 'highline/import'
 
-pwd = ask("Enter password: ") { |q| q.echo = false }
+module PowerSwitch
 
-#retrieve IP of power switch
-ip = File.open("#{File.expand_path(File.dirname(__FILE__))}/ip.txt","r").read
+  class Control < Struct.new(
+    :connection)
+    attr_reader :connection
+    #retrieve IP of power switch
+    @@ip=File.open("#{File.expand_path(File.dirname(__FILE__))}/ip.txt","r").read
 
+    def initialize
+      #need password
+      pwd = ask("Enter password: ") { |q| q.echo = false }
+      #open the connection
+      @connection = Net::Telnet::new(
+        "Host" => ip,
+        "Timeout" => 3,
+        "Prompt" => /[$%#>] \z/n
+      )
+      powerswitch.cmd(pwd) { |c| print c }
+    end
 
-powerswitch = Net::Telnet::new("Host" => ip,
-                             "Timeout" => 3,
-                             "Prompt" => /[$%#>] \z/n)
+    def close
+      @connection.cmd("/X") { |c| print c }
+      @connection.close
+    end
 
-powerswitch.cmd(pwd) { |c| print c }
-powerswitch.cmd("/X") { |c| print c }
-powerswitch.close
+    def cmd(comlist)
+      raise RuntimeError,"Can only deal with input argument 'comlist' as an Array, not class '#{comlist.class}'." unless comlist.is_a?(Array)
+      out=Array.new
+      comlist.each do |c|
+        out.push(@connection.cmd("/X") { |c| print c })
+      end
+      return out
+    end
+
+    def self.help
+      PowerSwitch::Control.new.cmd("/H").close
+    end
+
+    def status
+      self.cmd("")
+    end
+
+end
+
+#run status if this file is called explicitly
+if __FILE__==$0
+  puts PowerSwitch::Control.help
+end
