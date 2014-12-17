31166bd174fd6116112f7c5eb166b0108afe7deb
jgte <j.g.deteixeiradaencarnacao@tudelft.nl>
2014-12-17 18:01:33 +0100

    continued developing NPS-230.power_switch/libpowerswitch.rb

--- dev/NPS-230.power_switch/libpowerswitch.rb
+++ dev/NPS-230.power_switch/libpowerswitch.rb
@@ -14,10 +14,11 @@ module PowerSwitch
     @@comlist={
       :help    => "H",
       :status  => "S",
-      :general => "G",
-      :network => "N",
+      :general => "G\n\e",
+      :network => "N\n\e",
       :exit    => "X",
     }
+    @@power_modes=["On","Off","Boot"]
 
     def initialize
       #need password
@@ -54,10 +55,23 @@ module PowerSwitch
       return self
     end
 
+    def wait(seconds)
+      sleep(seconds)
+    end
+
+    def power(mode,plug)
+      raise RuntimeError,"Can only deal with input argument 'mode' as a " +
+          "String, not class '#{mode.class}'." unless mode.is_a?(String)
+      raise RuntimeError,"Can only deal with input argument 'plug' as a " +
+          "Integer, not class '#{plug.class}'." unless plug.is_a?(String)
+      raise RuntimeError,"Mode has to be one of #{@@power_modes.join(',')}, not '#{mode}'." unless @@power_modes.include?(mode)
+      self.cmd("#{mode} #{plug}")
+    end
+
   end
 end
 
 #run status if this file is called explicitly
 if __FILE__==$0
-  puts PowerSwitch::Control.new.cmd(:help).cmd(:status).close
+  puts PowerSwitch::Control.new.cmd(:help).cmd(:status).cmd(:general).cmd(:network).cmd(:status).close
 end
