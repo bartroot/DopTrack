6a1834e07ec30d8b5efbcd2dc416ff4b170ea30b
jgte <j.g.deteixeiradaencarnacao@tudelft.nl>
2015-09-15 11:09:27 +0200

    added script to mount bulk dir

---
+++ mount-bulk.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -u
+
+MOUNT_DIR=/home/doptrack/bulk
+
+if [ $# -lt 2 ]
+then
+  NETID=$LOGNAME
+else
+  NETID=$2
+fi
+
+if [ $# -lt 1 ]
+then
+  MODE=mount
+else
+  MODE=$1
+fi
+
+case $MODE in
+  mount|m)
+    if [ -z "$(ls $MOUNT_DIR)" ]
+    then
+      sshfs $NETID@sftp.tudelft.nl:/staff-bulk/lr/spe/as $MOUNT_DIR -o cache=no -o allow_other
+    else
+      echo "WARNING: $MOUNT_DIR is not empty, possible bulk is already mounted"
+    fi
+  ;;
+  umount|u)
+    if [ ! -z "$(ls $MOUNT_DIR)" ]
+    then
+      fusermount -u $MOUNT_DIR
+    else
+      echo "WARNING: $MOUNT_DIR is already empty, possible bulk is already unmounted"
+    fi
+  ;;
+  *)
+    echo "ERROR: unknown mode $MODE"
+  ;;
+esac
+
+
