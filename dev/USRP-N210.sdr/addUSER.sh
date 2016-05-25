0cfa654d21a37754ed43a6927d71e500f63f84c8
jgte <j.g.deteixeiradaencarnacao@tudelft.nl>
2016-05-25 15:02:01 +0200

    script for adding new users

---
+++ dev/USRP-N210.sdr/addUSER.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [ $# -ge 2 ]
+then
+	TARGET_GROUP=$2
+else
+	TARGET_GROUP=usrp
+fi
+
+if [ $# -ge 1 ]
+then
+	USER_NOW=$1
+else
+	USER_NOW=$USER
+fi
+
+echo Trying to add user $USER_NOW to group $TARGET_GROUP...
+
+#check if user exists
+awk -F':' '{print $1}' /etc/passwd | grep $USER_NOW > /dev/null
+if [ $? -ne 0 ]
+then
+	echo ERROR: unknown user $USER_NOW
+	exit 3
+fi
+
+#check if target group exists
+awk -F':' '{print $1}' /etc/group | grep $TARGET_GROUP > /dev/null
+if [ $? -ne 0 ]
+then
+	echo ERROR: unknown group $TARGET_GROUP
+	exit 3
+fi
+
+#check if user is already part of group
+GROUP_LIST=("$(groups $USER_NOW)")
+for i in $GROUP_LIST
+do
+	if [ "$i" == "$TARGET_GROUP" ]
+	then
+		echo User $USER_NOW already part of group $TARGET_GROUP.
+		exit 0
+	fi
+done
+
+#add user to target group
+sudo usermod -a -G $TARGET_GROUP $USER_NOW
+
