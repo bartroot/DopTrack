a449731e53d205315a1a310e011adad4b2c59a3c
msondergaard <sondergaard246@gmail.com>
2019-01-15 20:28:46 +0100

    Implement bugfixes to L1B processing script

--- GroundControl/Automation/process_L1B.py
+++ GroundControl/Automation/process_L1B.py
@@ -1,4 +1,6 @@
+""""Process DopTrack data to level 1B (time-frequency data)."""
 import logging
+import gc
 
 from doptools.io import Database
 from doptools.data import L1A, L1B, EmptyRecordingError
@@ -18,8 +20,8 @@ fh1 = logging.FileHandler(db.paths['logs'] / 'process_L1B.log')
 fh1.setLevel(logging.INFO)
 fh1.setFormatter(log_formatter)
 
-fh2 = logging.FileHandler(db.paths['logs'] / 'process_L1B_fail.log')
-fh2.setLevel(logging.WARNING)
+fh2 = logging.FileHandler(db.paths['logs'] / 'process_L1B_error.log')
+fh2.setLevel(logging.ERROR)
 fh2.setFormatter(log_formatter)
 
 logger.addHandler(fh1)
@@ -30,30 +32,33 @@ logger.addHandler(fh2)
 db = Database()
 dataids_to_process = db.dataids['L0'] - db.dataids['L1B'] - db.dataids['L1B_failed']
 
-
 logger.info(f'Processing {len(dataids_to_process)} data sets to level 1B')
 for dataid in sorted(dataids_to_process):
     logger.info(f'Processing {dataid}')
 
     try:
-        spec = L1A.create(dataid, nfft=250_000, dt=1)
-        path = db.paths['output'] / 'L1A' / f'{dataid}.png'
-        spec.plot(savepath=path)
-
-        data = L1B.create(spec)
-        data.save()
-        path = db.paths['output'] / 'L1B' / f'{dataid}.png'
-        data.plot(savepath=path, fit_func=False)
-
-        logger.info(f'Extraction of {dataid} succeded')
-        db.update_status(dataid, status='success')
-
+        spec = L1A.create(dataid, nfft=250_000, dt=0.1)
     except EmptyRecordingError as e:
         logger.warning(e)
         db.update_status(dataid, status='empty_recording')
+        continue
+
+    try:
+        data = L1B.create(spec)
+        data.save()
+        figpath = db.paths['output'] / 'L1B' / f'{dataid}.png'
+        data.plot(savepath=figpath, fit_func=False)
+        logger.info(f'Extraction of {dataid} succeded')
+        db.update_status(dataid, status='success')
     except PassNotFoundError as e:
+        figpath = db.paths['output'] / 'L1B_failed' / f'{dataid}.png'
+        spec.plot(savepath=figpath)
         logger.info(f'No pass was found in {dataid}: {e}')
         db.update_status(dataid, status='pass_not_found')
     except Exception:
+        figpath = db.paths['output'] / 'L1B_failed' / f'{dataid}.png'
+        spec.plot(savepath=figpath)
         logger.exception(f'Extraction of {dataid} failed unexpectedly')
         db.update_status(dataid, status='unknown_error')
+
+    gc.collect()  # Added to ensure that memory usage stays as low as possible
